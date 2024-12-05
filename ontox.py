4bdcf674a9c7818de99583f58414a32278e4d388
MkcDim <dimitri.mikec.pro@gmail.com>
2024-12-05 21:46:47 +0100

    improve code organisation

--- ontox.py
+++ ontox.py
@@ -15,7 +15,6 @@ def entity_relationship(onto_graph,onto_data,id_entity):
         if label!=id_entity
     }
 
-    # Identifier les frères/sœurs (même parent que start_label)
     start_id = onto_data.loc[onto_data['Preferred Label'] == id_entity, 'Class ID'].values[0]
     parent_ids = onto_data.loc[onto_data['Class ID'] == start_id, 'Parents'].values[0].split('|')
 
@@ -29,6 +28,22 @@ def entity_relationship(onto_graph,onto_data,id_entity):
     return relationships
 
 
+def build_onto_graph(onto_data) :
+
+    onto_graph = nx.DiGraph()
+
+    for index , row in onto_data.iterrows() :
+        id = row['Class ID']
+        pref_label = row["Preferred Label"]
+        if row['Parents'] : 
+            parents = row["Parents"].split("|")
+            for parent in parents : 
+                parent_label = onto_data.loc[onto_data['Class ID'] == parent, 'Preferred Label'].values
+                if len(parent_label) > 0:
+                    onto_graph.add_edge(parent_label[0], pref_label)
+
+
+    return onto_graph
 
 
 
@@ -46,39 +61,17 @@ def main() :
         print(f"ERROR : Incorrect path for the csv file")
         return
     
+    if args.id not in onto_df['Preferred Label'].values :
+        print(f"ERROR : id {args.id} doesn't exist in this csv file")
+        return    
+
     onto_df.drop_duplicates(inplace=True)
     onto_df['Parents'] = onto_df['Parents'].fillna("") # Replaces NaN values in the Parents column with “” to prepare for the eventual separation of the parents
 
-    onto_graph = nx.DiGraph()
-
-    #onto_df['Parents'] = onto_df['Parents'].apply(lambda x: x.split('|') if x else []) # Split Parents separated by "|" to get a table of parents for each Class ID
-    # Build dictionnary of onto_df : 
-
-    for index , row in onto_df.iterrows() :
-        id = row['Class ID']
-        pref_label = row["Preferred Label"]
-        if row['Parents'] : 
-            parents = row["Parents"].split("|")
-            for parent in parents : 
-                parent_label = onto_df.loc[onto_df['Class ID'] == parent, 'Preferred Label'].values
-                if len(parent_label) > 0:
-                    onto_graph.add_edge(parent_label[0], pref_label)
-
-
-       
-    nx.draw(onto_graph)
-    plt.savefig("onto_graph.png")
-
-    if args.id not in onto_df['Preferred Label'].values :
-        print(f"ERROR : id {args.id} doesn't exist in this csv file")
-        return
-
-
-    # The dictionary creates a key/value link between Class IDs and their labels and Parents, which will facilitate work on loops and data comparison queries
+    onto_graph = build_onto_graph(onto_df)
     relationship = entity_relationship(onto_graph,onto_df,args.id)
     sorted_relationships = dict(sorted(relationship.items(),key=itemgetter(1),reverse = True))
     print(sorted_relationships)
-
 
 if __name__ == "__main__":
     main()
