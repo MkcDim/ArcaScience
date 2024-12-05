f5ec38b4e9cd06dd9108a544b3aaffca9a8933de
MkcDim <dimitri.mikec.pro@gmail.com>
2024-12-05 13:35:15 +0100

    add argpare API

--- ontox.py
+++ ontox.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import os
+import argparse
+from operator import itemgetter
 
 # Function to find each relationship, and the depth of each relationship.
 def entity_relationship(onto_dict,id_entity,depth = 0, known_relationship = None):
@@ -27,26 +29,40 @@ def entity_relationship(onto_dict,id_entity,depth = 0, known_relationship = None
 
 
 
+def main() : 
 
-work_path = os.getcwd()
-data_path = os.path.join(work_path,'data')
-file_path = os.path.join(data_path,'onto_x.csv')
+    parser = argparse.ArgumentParser(description = "Onto-X Project")
+    parser.add_argument("--csv",type=str,default=r"data\\onto_x.csv",help = "Path to Onto-X Project CSV file. Default: onto_x.csv")
+    parser.add_argument("--id",type=str,default='http://entity/CST/GYNCERV',help="ID of the entity we want its relationships")
+    args = parser.parse_args()
+
+    try :
+        onto_df = pd.read_csv(args.csv,sep=',')
+    except FileNotFoundError : 
+        print(f"ERROR : Incorrect path for the csv file")
+        return
+    
+    onto_df.drop_duplicates(inplace=True)
+    onto_df['Parents'] = onto_df['Parents'].fillna("") # Replaces NaN values in the Parents column with “” to prepare for the eventual separation of the parents
+    onto_df['Parents'] = onto_df['Parents'].apply(lambda x: x.split('|') if x else []) # Split Parents separated by "|" to get a table of parents for each Class ID
+
+    # Build dictionnary of onto_df : 
+    onto_dict = {
+        row['Class ID']: {"label":row["Preferred Label"], "parents" : row["Parents"]}
+        for index, row in onto_df.iterrows()
+    } 
 
 
-# Load Data
-onto_df = pd.read_csv(file_path,sep=',')
+    if args.id not in onto_dict :
+        print(f"ERROR : id {args.id} doesn't exist in this csv file")
+        return
 
 
-onto_df['Parents'] = onto_df['Parents'].fillna("") # Replaces NaN values in the Parents column with “” to prepare for the eventual separation of the parents
-onto_df['Parents'] = onto_df['Parents'].apply(lambda x: x.split('|') if x else []) # Split Parents separated by "|" to get a table of parents for each Class ID
+    # The dictionary creates a key/value link between Class IDs and their labels and Parents, which will facilitate work on loops and data comparison queries
+    relationship = entity_relationship(onto_dict,args.id)
+    sorted_relationships = dict(sorted(relationship.items(),key=itemgetter(1),reverse = True))
+    print(sorted_relationships)
 
-# Build dictionnary of onto_df : 
-onto_dict = {
-    row['Class ID']: {"label":row["Preferred Label"], "parents" : row["Parents"]}
-    for index, row in onto_df.iterrows()
-} 
-# The dictionary creates a key/value link between Class IDs and their labels and Parents, which will facilitate work on loops and data comparison queries
 
-example_of_entity = 'http://entity/CST/HYPOCHLOREM'
-relationship = entity_relationship(onto_dict,example_of_entity)
-print(relationship)
+if __name__ == "__main__":
+    main()
