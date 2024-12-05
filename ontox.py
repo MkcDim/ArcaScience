4ff5aa9fa29f36f65e560ac6812bc70d7dbe780e
MkcDim <dimitri.mikec.pro@gmail.com>
2024-12-05 10:48:27 +0100

    create onto dictionnary

--- ontox.py
+++ ontox.py
@@ -15,3 +15,5 @@ onto_df = pd.read_csv(file_path,sep=',')
 
 onto_df['Parents'] = onto_df['Parents'].fillna("") # Replaces NaN values in the Parents column with “” to prepare for the eventual separation of the parents
 onto_df['Parents'] = onto_df['Parents'].apply(lambda x: x.split('|') if x else []) # Split Parents separated by "|" to get a table of parents for each Class ID
+
+# Build dictionnary of onto_df : 
