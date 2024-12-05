bfc93257c21587c0f811ed240c2a15d0a58ea774
MkcDim <dimitri.mikec.pro@gmail.com>
2024-12-05 11:27:08 +0100

    function to find relationships, first version

--- ontox.py
+++ ontox.py
@@ -1,6 +1,19 @@
 import pandas as pd
 import os
 
+# Function to find each relationship, and the depth of each relationship.
+def entity_relationship(onto_dict,id_entity,depth = 0):
+
+    entity = onto_dict.get(id_entity)
+
+    relationships = {entity['label'] : depth} #dictionnary of each relationship between label and depth of relation ships
+
+    # Look for depth relationship with recursive fonction
+    for parents_id in entity['parents'] :
+        relationships.update(entity_relationship(onto_dict,parents_id,depth =+ 1))
+
+    return relationships
+
 
 
 
@@ -17,3 +30,11 @@ onto_df['Parents'] = onto_df['Parents'].fillna("") # Replaces NaN values in the 
 onto_df['Parents'] = onto_df['Parents'].apply(lambda x: x.split('|') if x else []) # Split Parents separated by "|" to get a table of parents for each Class ID
 
 # Build dictionnary of onto_df : 
+onto_dict = {
+    row['Class ID']: {"label":row["Preferred Label"], "parents" : row["Parents"]}
+    for index, row in onto_df.iterrows()
+} 
+# The dictionary creates a key/value link between Class IDs and their labels and Parents, which will facilitate work on loops and data comparison queries
+
+example_of_entity = 'http://entity/CST/HYPOCHLOREM'
+relationship = entity_relationship(onto_dict,example_of_entity)
